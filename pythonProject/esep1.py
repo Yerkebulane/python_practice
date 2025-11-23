388a3baa8f631fea1e88966bd641c96b5e9077a8
Yerkebulane <qassymbekerkebulan@gmail.com>
2025-11-23 20:29:51 +0500

    esep

--- pythonProject/esep1.py
+++ pythonProject/esep1.py
@@ -1,5 +1,15 @@
-if __name__ == '__main__':
-    n = int(input())
-    for i in range(n):
-        if i>=0:
-            print(i**2)
+# if __name__ == '__main__':
+#     n = int(input())
+#     for i in range(n):
+#         if i>=0:
+#             print(i**2)
+
+n = int(input())
+
+arr=[]
+
+for i in range(n):
+    elem = int(input('elem' + i + ': '))
+    arr.append(elem)
+    
+print(arr)
